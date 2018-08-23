bea5157972ca9bf0b5c1101f29adc2fe2dc7b921
João Marques <jonny1995@live.com.pt>
2018-08-23 19:54:44 +0100

    First Commit

---
+++ src/components/facerecon/facerecon.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './facerecon.css';
+
+const FaceRecon = ({ imageUrl, boxes }) => {
+    return (
+      <div className='center ma'>
+        <div className='absolute mt2'>
+          <img id='inputimage' alt='' src={imageUrl} width='500px' heigh='auto'/>
+          {boxes.map(box => {
+          return <div key={box.topRow} className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
+            })
+            }
+        </div>
+      </div>
+    );
+  }
+
+export default FaceRecon;
